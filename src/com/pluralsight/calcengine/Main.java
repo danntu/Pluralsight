22e9fa94a5c544d06bcb7c92000f3d353c9e13ed
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2017-11-20 14:07:09 +0600

    CalcEngine with Arrays,Loop, and Switch

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -2,25 +2,44 @@ package com.pluralsight.calcengine;
 
 public class Main {
     public static void main(String[] args) {
-        double val1=100.0d,val2=0.0d,result;
-        char opCode='d';
+        double[] leftVals = {100.0d, 25.0d,225.0d,11.0d};
 
-        if (opCode=='a')
-            result=val1+val2;
-        else if (opCode=='s')
-            result=val1-val2;
-        else if (opCode=='d')
-            result= val2!=0 ? val1/val2 : 0.0d;
+        double[] rigthVal={50.0d,92.0d,17.0d,3.0d};
+
+        char[] opCodes={'d','a','s','m'};
+        double[] result =new double[opCodes.length];
+
+        for (int i = 0; i <opCodes.length ; i++) {
+            switch (opCodes[i]) {
+                case 'a':
+                    result[i] = leftVals[i] + rigthVal[i];
+                    break;
+                case 's':
+                    result[i] = leftVals[i] - rigthVal[i];
+                    break;
+                case 'd':
+                    result[i] = rigthVal[i] != 0.0d ? leftVals[i] / rigthVal[i] : 0.0d;
 //            if (val2 !=0)
 //              result=val1/val2;
 //            else
 //              result=0.0d;
-        else if (opCode=='m')
-            result=val1*val2;
-        else {
-            System.out.println("Error - invalid opCode");
-            result = 0.0d;
+                    break;
+                case 'm':
+                    result[i] = leftVals[i] * rigthVal[i];
+                    break;
+                default:
+                    System.out.println("Error - invalid opCode");
+                    result[i] = 0.0d;
+                    break;
+                }
+            }
+
+        for (double theResult:
+             result) {
+            System.out.print("result = ");
+            System.out.println(theResult);
         }
-        System.out.println(result);
+
+
     }
 }
