41f809f9429f18f3f4132255756902eb7d561c3c
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2017-11-20 11:49:26 +0600

    Using comment single and multi

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
-
+/*
+This is the first line of a comment
+This is the second line of a comment
+//line comment
+ */
     public static void main(String[] args) {
-        System.out.println("Hello World Again");
-        System.out.println( "Hello World Again" );
-        System.out.println(   "Hello World Again") ;
-        System.out.println
-                (
-        "Hello World Again"
-                          )
-        ;
+        //This is a stand alone comment
+        System.out.println("Hello World One"); // This is a comment
+        //System.out.println("Hello World Two");
+        System.out.println(/*"Hello World Three"*/"This is different text");
     }
 }
