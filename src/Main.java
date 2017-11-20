f01c3406047973e2b940129403d36d710550fe03
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2017-11-20 11:41:32 +0600

    Using whitespaces in System.out.println()

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World Again");
+        System.out.println( "Hello World Again" );
+        System.out.println(   "Hello World Again") ;
+        System.out.println
+                (
+        "Hello World Again"
+                          )
+        ;
     }
 }
