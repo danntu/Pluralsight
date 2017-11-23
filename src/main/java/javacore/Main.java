8eec5378257c3f6e2921e04a4697c990bb00cf3c
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2017-11-23 11:01:15 +0600

    Java Core try/catch/finally. Try with resources

--- src/main/java/javacore/Main.java
+++ src/main/java/javacore/Main.java
@@ -2,20 +2,25 @@ package javacore;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.io.Writer;
+
+import static javacore.Helper.*;
 
 public class Main {
     public static void main(String[] args) {
         //doTryCatchFinally();
-        doCloseThing();
+        //doCloseThing();
+        //doTryWithResources();
+        doTryWithResourcesMulti();
     }
 
     public static void doTryCatchFinally(){
         char[] buff = new char[8];
-
         int length;
+
         Reader reader =null;
         try{
-            reader = Helper.openReader("file1.txt");
+            reader = openReader("file1.txt");
             while ((length=reader.read(buff))>=0){
                 System.out.println("\nlength: "+length);
                 for (int i = 0; i < length ; i++) {
@@ -36,11 +41,32 @@ public class Main {
     }
 
     public static void doTryWithResources(){
-
+        char[] buff = new char[8];
+        int length;
+        try(Reader reader = openReader("file1.txt")){
+            while ((length=reader.read(buff))>=0){
+                System.out.println("\nlength: "+length);
+                for (int i = 0; i < length ; i++) {
+                    System.out.print(buff[i]);
+                }
+            }
+        } catch (IOException e){
+            System.out.println(e.getClass().getSimpleName()+" - "+e.getMessage());
+        }
     }
 
     public static void doTryWithResourcesMulti(){
-
+        char[] buff = new char[8];
+        int length;
+        try(Reader reader = openReader("file1.txt");
+            Writer writer = openWriter("file2.txt")){
+            while ((length=reader.read(buff))>=0){
+                System.out.println("\nlength: "+length);
+                writer.write(buff,0,length);
+            }
+        } catch (IOException e){
+            System.out.println(e.getClass().getSimpleName()+" - "+e.getMessage());
+        }
     }
 
     private static void doCloseThing(){
