978dacb92a69e53fda2d5a1fdbd978ea1b9e328c
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-01-04 14:22:48 +0600

    Lambda expression System.out::println

--- src/main/java/java8/ComporatorLambda.java
+++ src/main/java/java8/ComporatorLambda.java
@@ -1,6 +1,7 @@
 package java8;
 
 import java.util.*;
+import java.util.function.Consumer;
 
 public class ComporatorLambda {
     public static void main(String[] args) {
@@ -11,8 +12,9 @@ public class ComporatorLambda {
             }
         };
 
-        Comparator<String> comp2 = (String s1,String s2)->
-                Integer.compare(s1.length(),s2.length());
+        Comparator<String> comp2 = (String s1,String s2)->Integer.compare(s1.length(),s2.length());
+        Comparator<Integer> comp3 = Integer::compare;
+
         System.out.println("Simple");
         List<String> list1 = Arrays.asList("***","**","****","*");
         Collections.sort(list1,comp1);
@@ -27,5 +29,22 @@ public class ComporatorLambda {
                 list2) {
             System.out.println(s);
         }
+
+        List<Integer> list3 = Arrays.asList(7,3,4,6);
+
+        Collections.sort(list3,comp3);
+        System.out.println("Lambda without type with collection s-> System.out.print(s+\" \")");
+        list3.forEach(s-> System.out.print(s+" "));
+
+        System.out.println("Lambda without type with collection System.out::print()");
+        list3.forEach(System.out::print);
+
+        System.out.println("\nLambda without type");
+        for (Integer s:
+                list3) {
+            System.out.print(s+" ");
+        }
+
+
     }
 }
