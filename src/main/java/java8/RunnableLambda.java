9dbaf7cd9151e28443d77dd9e226b9c8a8ae6a86
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-01-04 12:55:58 +0600

    Lambda expression Runnable

---
+++ src/main/java/java8/RunnableLambda.java
@@ -0,0 +1,36 @@
+package java8;
+
+public class RunnableLambda {
+    public static void main(String[] args) throws InterruptedException {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 3; i++) {
+                    System.out.println(
+                            "Hello world from thread [" +
+                                    Thread.currentThread().getName()+
+                                    "]");
+
+                }
+            }
+        };
+
+        Runnable runnableLambda= ()->{
+            for (int i = 0; i < 3; i++) {
+                System.out.println(
+                        "Hello world from thread [" +
+                                Thread.currentThread().getName()+
+                                "]");
+
+            }
+        };
+
+        Thread t1  = new Thread(runnableLambda);
+        t1.start();
+        t1.join();
+
+        Thread t2  = new Thread(runnable);
+        t2.start();
+        t2.join();
+    }
+}
