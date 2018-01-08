84c8e67287996fcad02e6e745ecd032f7a5070de
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-01-08 09:48:53 +0600

    Live Coding with Intermediary and Terminal operations

---
+++ src/main/java/java8/IntermediaryAndFinal.java
@@ -0,0 +1,26 @@
+package java8;
+
+import com.sun.org.apache.xalan.internal.xsltc.dom.SortingIterator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
+public class IntermediaryAndFinal {
+    public static void main(String[] args) {
+        Stream<String> stream = Stream.of("one","two","three","four","five");
+
+        Predicate<String> p1 = Predicate.isEqual("two");
+        Predicate<String> p2 = Predicate.isEqual("three");
+
+        List<String> list = new ArrayList<>();
+        stream
+                .peek(System.out::println)
+                .filter(p1.or(p2))
+                .forEach(list::add);
+
+        System.out.println("Done!");
+        System.out.println("Size = " +list.size());
+    }
+}
