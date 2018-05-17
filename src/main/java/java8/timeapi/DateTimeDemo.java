9aaa730a16969c895bd9c94f918b0e510f4acda4
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-05-17 09:18:06 +0600

    Java 8. Using LocalDate and LocalTime

---
+++ src/main/java/java8/timeapi/DateTimeDemo.java
@@ -0,0 +1,18 @@
+package java8.timeapi;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class DateTimeDemo {
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+
+        LocalTime localTime = LocalTime.now();
+        System.out.println(localTime);
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+    }
+}
